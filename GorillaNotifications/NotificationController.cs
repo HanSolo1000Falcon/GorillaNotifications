def9e864552f3562fff37fc83a8fd975a50e57b3
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-02-13 00:08:18 +0100

    Refactor `NotificationController`: Remove `Extensions` and redesign notification management with reusable text pooling and font customization.

--- GorillaNotifications/NotificationController.cs
+++ GorillaNotifications/NotificationController.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using GorillaLocomotion;
-using GorillaNotifications.Tools;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,11 +11,14 @@ namespace GorillaNotifications;
 
 public class NotificationController : MonoBehaviour
 {
+    private const float PCHeightOffset = 53.065f;
+    private const float VRHeightOffset = 0.054f;
+    
     private static   NotificationController  instance;
     private readonly List<NotificationEntry> notificationEntries = [];
-    private          TextMeshProUGUI         pcScreenNotificationText;
+    private readonly List<TextPoolEntry> textPool = [];
 
-    private Text worldSpaceNotificationText;
+    private readonly Dictionary<FontType, (Font, TMP_FontAsset)> fontCache = [];
 
     private void Awake() => instance = this;
 
@@ -31,52 +32,97 @@ public class NotificationController : MonoBehaviour
         GameObject worldSpaceCanvas = Instantiate(bundle.LoadAsset<GameObject>("WorldSpaceCanvas"),
                 GTPlayer.Instance.headCollider.transform);
 
-        worldSpaceCanvas.transform.localPosition = new Vector3(-0.25f, -0.2f, 1f);
-        worldSpaceCanvas.transform.localRotation = Quaternion.Euler(20f, -20f, 0f);
+        worldSpaceCanvas.transform.localPosition = new Vector3(0f, 0f, 1f);
+        worldSpaceCanvas.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        
         worldSpaceCanvas.SetLayer(UnityLayer.FirstPersonOnly);
-        worldSpaceNotificationText               = worldSpaceCanvas.GetComponentInChildren<Text>();
 
         GameObject pcScreenCanvas = Instantiate(bundle.LoadAsset<GameObject>("PCScreenCanvas"));
-        pcScreenNotificationText = pcScreenCanvas.GetComponentInChildren<TextMeshProUGUI>();
-    }
-
-    private IEnumerator RemoveNotification(NotificationEntry entry)
-    {
-        yield return new WaitForSeconds(entry.Duration);
-        notificationEntries.Remove(entry);
-        RefreshText();
-    }
-
-    private void RefreshText()
-    {
-        worldSpaceNotificationText.text = notificationEntries
-                                         .Select(entry =>
-                                                         $"[{entry.Source}] {entry.Notification}".NormaliseNotification(
-                                                                 50)).Join("\n");
-
-        pcScreenNotificationText.text = notificationEntries
-                                       .Select(entry => $"[{entry.Source}] {entry.Notification}".NormaliseNotification(
-                                                       Screen.width.MapForScreenWidth())).Join("\n");
+        
+        TextPoolEntry textPoolEntry = new(worldSpaceCanvas.GetComponentInChildren<Text>(), pcScreenCanvas.GetComponentInChildren<TextMeshProUGUI>());
+        textPool.Add(textPoolEntry);
+        
+        fontCache.Add(FontType.JetBrains_Mono, (bundle.LoadAsset<Font>("JetBrainsMono-ExtraBold"), bundle.LoadAsset<TMP_FontAsset>("JetBrainsMono-ExtraBold SDF")));
+        fontCache.Add(FontType.Bit_Cell, (bundle.LoadAsset<Font>("bitcell_memesbruh03"), bundle.LoadAsset<TMP_FontAsset>("bitcell_memesbruh03 SDF")));
+        fontCache.Add(FontType.VCR_OSD_Mono, (bundle.LoadAsset<Font>("VCR_OSD_MONO"), bundle.LoadAsset<TMP_FontAsset>("VCR_OSD_MONO SDF")));
+        fontCache.Add(FontType.Pix32, (bundle.LoadAsset<Font>("Pix32"), bundle.LoadAsset<TMP_FontAsset>("Pix32 SDF")));
+        fontCache.Add(FontType.Pixellari, (bundle.LoadAsset<Font>("Pixellari"), bundle.LoadAsset<TMP_FontAsset>("Pixellari SDF")));
     }
 
     /// <summary>
-    ///     Sends a notification to be displayed with the specified source, message, and duration.
+    /// Sends a notification with the specified source, message, duration, and font type.
     /// </summary>
     /// <param name="source">The source of the notification, indicating where or who the notification originated from.</param>
     /// <param name="notification">The message or content of the notification.</param>
     /// <param name="duration">The duration (in seconds) that the notification will remain visible.</param>
-    /// <returns>
-    ///     A <see cref="NotificationEntry" /> object that represents the notification and allows further updates or
-    ///     removal.
-    /// </returns>
-    public static NotificationEntry SendNotification(string source, string notification, float duration) =>
-            new(source, notification, duration);
+    /// <param name="fontType">The font type used for the notification. Defaults to JetBrainsMono.</param>
+    /// <returns>A new instance of <see cref="NotificationController.NotificationEntry"/> representing the created notification.</returns>
+    public static NotificationEntry SendNotification(string   source, string notification, float duration, FontType fontType = FontType.JetBrains_Mono) =>
+            new(source, notification, duration, instance.GetAvailableTextPoolEntry(), fontType);
+
+    private TextPoolEntry GetAvailableTextPoolEntry()
+    {
+        if (textPool.Count > 1)
+        {
+            TextPoolEntry entry = textPool[^1];
+            textPool.RemoveAt(textPool.Count - 1);
+            return entry;
+        }
+
+        Text            vrText = Instantiate(textPool[0].Text,     textPool[0].Text.transform.parent);
+        TextMeshProUGUI pcText = Instantiate(textPool[0].TextMesh, textPool[0].TextMesh.transform.parent);
+
+        return new TextPoolEntry(vrText, pcText);
+    }
+
+
+    private void ReorderTextLines()
+    {
+        for (int i = notificationEntries.Count - 1; i >= 0; i--)
+        {
+            int iMapped = notificationEntries.Count - i - 1;
+            notificationEntries[i].AssociatedTextPoolEntry.Text.transform.localPosition =
+                    new Vector3(-0.5f, iMapped * VRHeightOffset - 0.5f, 0f);
+            notificationEntries[i].AssociatedTextPoolEntry.TextMesh.transform.position =
+                    new Vector3(0f, iMapped * PCHeightOffset, 0f);
+        }
+    }
+
+    internal struct TextPoolEntry(Text text, TextMeshProUGUI textMesh)
+    {
+        public readonly Text Text = text;
+        public readonly TextMeshProUGUI TextMesh = textMesh;
+    }
+
+    private IEnumerator NotificationLifetimeRoutine(NotificationEntry entry)
+    {
+        while (entry.Duration > -1f)
+        {
+            if (entry.HasBeenDestroyed)
+                yield break;
+            
+            entry.Duration -= Time.deltaTime;
+
+            if (entry.Duration <= 0f)
+            {
+                entry.AssociatedTextPoolEntry.Text.color = new Color(1f, 1f, 1f, 1f + entry.Duration);
+                entry.AssociatedTextPoolEntry.TextMesh.color = new Color(1f, 1f, 1f, 1f + entry.Duration);
+            }
+            else
+            {
+                entry.AssociatedTextPoolEntry.Text.color = Color.white;
+                entry.AssociatedTextPoolEntry.TextMesh.color = Color.white;
+            }
+            
+            yield return null;
+        }
+        
+        entry.RemoveNotification();
+    }
 
     public sealed class NotificationEntry
     {
-        private Coroutine removeRoutine;
-
-        internal NotificationEntry(string source, string notification, float duration)
+        internal NotificationEntry(string source, string notification, float duration, TextPoolEntry textPoolEntry, FontType fontType)
         {
             if (notification.IsNullOrEmpty())
                 return;
@@ -84,14 +130,25 @@ public class NotificationController : MonoBehaviour
             Source        = source;
             Notification  = notification;
             Duration      = duration;
-            removeRoutine = instance.StartCoroutine(instance.RemoveNotification(this));
+            AssociatedTextPoolEntry = textPoolEntry;
+            
+            textPoolEntry.Text.text = $"[{Source}] {Notification}";
+            textPoolEntry.TextMesh.text = $"[{Source}] {Notification}";
+            
+            textPoolEntry.Text.font = instance.fontCache[fontType].Item1;
+            textPoolEntry.TextMesh.font = instance.fontCache[fontType].Item2;
+            
             instance.notificationEntries.Add(this);
-            instance.RefreshText();
+            instance.ReorderTextLines();
+            instance.StartCoroutine(instance.NotificationLifetimeRoutine(this));
         }
 
-        public string Source       { get; private set; }
-        public string Notification { get; private set; }
-        public float  Duration     { get; private set; }
+        public string Source       { get; internal set; }
+        public string Notification { get; internal set; }
+        public float  Duration     { get; internal set; }
+
+        internal bool HasBeenDestroyed;
+        internal TextPoolEntry AssociatedTextPoolEntry;
 
         /// <summary>
         ///     Updates the notification with new source, message, and duration values.
@@ -101,12 +158,12 @@ public class NotificationController : MonoBehaviour
         /// <param name="newDuration">The new duration (in seconds) that the notification will remain visible.</param>
         public void UpdateNotification(string newSource, string newNotification, float newDuration)
         {
-            instance.StopCoroutine(removeRoutine);
             Source        = newSource;
             Notification  = newNotification;
             Duration      = newDuration;
-            instance.RefreshText();
-            removeRoutine = instance.StartCoroutine(instance.RemoveNotification(this));
+            
+            AssociatedTextPoolEntry.Text.text     = $"[{Source}] {Notification}";
+            AssociatedTextPoolEntry.TextMesh.text = $"[{Source}] {Notification}";
         }
 
         /// <summary>
@@ -132,9 +189,20 @@ public class NotificationController : MonoBehaviour
         /// </summary>
         public void RemoveNotification()
         {
-            instance.StopCoroutine(removeRoutine);
             instance.notificationEntries.Remove(this);
-            instance.RefreshText();
+            instance.textPool.Add(AssociatedTextPoolEntry);
+            AssociatedTextPoolEntry.Text.text = "";
+            AssociatedTextPoolEntry.TextMesh.text = "";
+            HasBeenDestroyed = true;
         }
+    }
+
+    public enum FontType
+    {
+        JetBrains_Mono,
+        Bit_Cell,
+        VCR_OSD_Mono,
+        Pix32,
+        Pixellari,
     }
 }
