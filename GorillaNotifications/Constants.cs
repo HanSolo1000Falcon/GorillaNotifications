9826484aaeced2261dee176b6f4aa5812b866909
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-02-14 16:22:21 +0100

    bump ver

--- GorillaNotifications/Constants.cs
+++ GorillaNotifications/Constants.cs
@@ -4,5 +4,5 @@ public static class Constants
 {
     public const string PluginGuid    = "hansolo1000falcon.gorillanotifications";
     public const string PluginName    = "GorillaNotifications";
-    public const string PluginVersion = "1.0.0";
+    public const string PluginVersion = "1.0.2";
 }
